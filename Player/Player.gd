fcd8b4ea709932a20347e3a17cdef15ebb8280cf
0362777 <0362777@schoolsnet.act.edu.au>
2022-04-07 19:25:30 +1000

    code
    I had to copy paste my code from my previous one as.

--- Player/Player.gd
+++ Player/Player.gd
@@ -14,10 +14,18 @@ func _ready():
 	set_physics_process(true)
 
 func _physics_process(delta):
-	pass
+	if Input.is_action_pressed("ui_up"):
+		if position.y >0: 
+			move_and_collide(Vector2(0, -movement_speed * delta))
+	if Input.is_action_pressed("ui_down"):
+		if position.y < 1000: 
+			move_and_collide(Vector2(0, movement_speed * delta))
 			
 func _process(delta):
-	pass
+	if Input.is_action_just_pressed("fire"):
+		var bulletInstance = bulletSource.instance()
+		bulletInstance.position = Vector2(position.x, position.y-20)
+		get_tree().get_root().add_child(bulletInstance)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
